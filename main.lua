ffac443fe7131dd84c59c0615135481acc96a23c
bwdevel <bwdevel@yahoo.com>
2012-08-27 11:01:30 +0200

    readding main.lua

---
+++ main.lua
@@ -0,0 +1,84 @@
+--      ==          == == ==       ==       == ==
+--      ==          ==    ==    ==    ==    ==    ==
+--      ==          ==    ==    == == ==    ==    ==
+--      ==          ==    ==    ==    ==    ==    ==
+--      == == ==    == == ==    ==    ==    == ==    
+
+function love.load()
+	-- load basic modules
+	require("resources/entities")
+	require("resources/entityFactory")
+	ents.Startup()
+
+	local ent=ents.Create("player", 200,200)
+	
+end
+
+
+--      ==    ==    == ==       == ==          ==       == == ==    == == ==
+--      ==    ==    ==    ==    ==    ==    ==    ==       ==       ==
+--      ==    ==    == ==       ==    ==    == == ==       ==       == ==
+--      ==    ==    ==          ==    ==    ==    ==       ==       ==
+--      == == ==    ==          == ==       ==    ==       ==       == == ==
+
+function love.update(dt)
+	-- Update factory
+	entFactory.update(dt)
+	
+	-- Update entities
+	ents:update(dt)
+end
+
+
+--     == ==       == ==          ==       ==    ==
+--     ==    ==    ==    ==    ==    ==    ==    ==
+--     ==    ==    == ==       == == ==    ==    ==
+--     ==    ==    ==   ==     ==    ==    == == ==
+--     == ==       ==   ==     ==    ==    ==    ==
+
+function love.draw()
+	-- Draw entities
+	ents:draw()
+end
+
+function love.focus(bool)
+end
+
+
+--      == == ==    ==    ==    == ==       ==    ==    == == ==
+--         ==       == == ==    ==    ==    ==    ==       ==
+--         ==       == == ==    == ==       ==    ==       ==
+--         ==       ==    ==    ==          ==    ==       ==
+--      == == ==    ==    ==    ==          == == ==       ==
+
+function love.keypressed(key, unicode)
+	-- Notify entity of keypressed event
+	ents:keypressed(key, unicode)
+end
+
+function love.keyreleased(key, unicode)
+	-- Notify entity of keyreleased event
+	ents:keyreleased(key, unicode)
+end
+
+function love.mousepressed(x, y, button)
+	-- Notify entity of mousepressed event
+	ents:mousepressed(x, y, button)
+end
+
+function love.mousereleased(x, y, button)
+end
+
+
+--      == == ==    ==    ==    == == ==    == == ==
+--      ==    ==    ==    ==       ==          ==
+--      ==    ==    ==    ==       ==          ==
+--      == == ==    ==    ==       ==          ==
+--            ==    == == ==    == == ==       ==
+
+function love.quit()
+end
+
+
+
+
