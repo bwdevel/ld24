27a6ef50e53874ad060763c92a40d98a1080aa36
bwdevel <bwdevel@yahoo.com>
2012-08-27 12:48:57 +0200

    updated main.lua to support background fading (prototyped)

--- main.lua
+++ main.lua
@@ -32,6 +32,8 @@ function love.update(dt)
 	
 	-- Update entities
 	ents:update(dt)
+
+	tempUpdate(dt)
 end
 
 
@@ -60,6 +62,9 @@ end
 function love.keypressed(key, unicode)
 	-- Notify entity of keypressed event
 	ents:keypressed(key, unicode)
+
+	tempKeyPress(key, unicode)
+
 end
 
 function love.keyreleased(key, unicode)
@@ -85,24 +90,57 @@ end
 function love.quit()
 end
 
-
----- everything below this should probably be moved into something useful
-
+--------------------------------------------------------------------------------------
+---- everything below this should probably be moved to it's home after prototyping ---
+--------------------------------------------------------------------------------------
 function tempInit()
-print("init works")
+	print("init works")
 
-imgBackground = { }
 
-imgBackground[1] = love.graphics.newImage("resources/images/background_a.png")
-imgBackground[2] = love.graphics.newImage("resources/images/background_b.png")
-imgBackground[3] = love.graphics.newImage("resources/images/background_c.png")
+	imgBackground = { }
 
+	imgBackground[1] = love.graphics.newImage("resources/images/background_a.png")
+	imgBackground[2] = love.graphics.newImage("resources/images/background_b.png")
+	imgBackground[3] = love.graphics.newImage("resources/images/background_c.png")
+
+	bgCurrent = 1
+	bgOld = #imgBackground
+	bgFade = false
+	bgFadeRate = 255
+	bgFadeLevel = 255
 
 end
 
+function tempUpdate(dt)
+	if bgFade == true then
+		bgFadeLevel = bgFadeLevel - bgFadeRate*dt
+		if bgFadeLevel < 0 then
+			bgFadeLevel = 255
+			bgFade = false
+		end
+	end
+
+end
 
 function tempDraw()
+	love.graphics.setBackgroundColor(0,0,0)
 
 	love.graphics.setColor(255,255,255,255)
-	love.graphics.draw(imgBackground[3],0,0)
+	love.graphics.draw(imgBackground[bgCurrent],0,0)
+
+	if bgFade == true then
+		love.graphics.setColor(255,255,255,bgFadeLevel)
+		love.graphics.draw(imgBackground[bgOld],0,0)
+	end
 end
+
+function tempKeyPress(key, unicode)
+	if key == "5" and bgFade == false then
+		bgOld = bgCurrent
+		bgCurrent = bgCurrent + 1
+		if bgCurrent > #imgBackground then bgCurrent = 1 end
+		bgFade = true
+	end
+
+end
+
