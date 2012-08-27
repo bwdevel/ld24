c6f910e06d8295e2a87501460ef14262f9875d6c
Innidael <Innidael@gmail.com>
2012-08-28 00:04:57 +0200

    Changed entityFactory to support continues recipes
    Changed entityFactory to support "special" recipes. When amount is set
    to -1 the recipe continues to spawn entities until the factory is reset
    with the reset functionality.

--- resources/entityFactory.lua
+++ resources/entityFactory.lua
@@ -1,6 +1,6 @@
 entFactory = {}
 entFactory.recipes = {}
-local recipe = {}
+--local recipe = {}
 
 function entFactory.update(dt)
 	entFactory.processRecipes()
@@ -9,32 +9,48 @@ end
 -- Add a recipe to the recipe list
 function entFactory.addRecipe(entityName, amount, spawnInterval, spawnArea)
 	if ents.isRegisteredEntity(entityName) then
-		if amount > 0 then
-			if spawnInterval > 0 then
-				if spawnArea.x >= 0 and spawnArea.y >= 0 and spawnArea.w <= love.graphics.getWidth() and spawnArea.h <= love.graphics.getHeight() then
-					recipe = {entity=entityName, amount=amount, spawnInterval=spawnInterval, spawnCountDown=spawnInterval, spawnArea=spawnArea}
-					table.insert(entFactory.recipes, recipe)
+		if spawnInterval > 0 then
+			if entFactory.validateSpawnArea(spawnArea) then
+				if amount > 0 or amount == -1 then
+					-- amount == -1 is a special case: unlimited enemies: use reset to stop the factory			
+					entFactory.createRecipe(entityName, amount, spawnInterval, spawnArea)
 					--print("Added recipe: entity[" .. entityName .. "] amount[" .. amount .. "] interval[" .. spawnInterval .. "] area.x[" .. spawnArea.x .. "] area.y[" .. spawnArea.y .. "] area.w[" .. spawnArea.w .. "] area.h[" .. spawnArea.h .. "]")
 				else
-					print("Error: Spawn area incorrectly specified!")
+					print("Error: No amount specified for recipe!")
 				end
 			else
-				print("Error: Invalid spawninterval specified!")
+				print("Error: Spawn area incorrectly specified!")
 			end
 		else
-			print("Error: No amount specified for recipe!")
+			print("Error: Invalid spawninterval specified!")
 		end
 	else
 		print("Error: Entity not registered!")
 	end
 end
 
+-- Create recipe
+function entFactory.createRecipe(entityName, amount, spawnInterval, spawnArea)
+	local recipe = {entity=entityName, amount=amount, spawnInterval=spawnInterval, spawnCountDown=spawnInterval, spawnArea=spawnArea};
+	table.insert(entFactory.recipes, recipe)
+end
+
 -- Remove a recipe from the recipe list
 function entFactory.delRecipe(recipeId)
-	if recipeId >= #recipe and recipeId <= #recipe then
+	if recipeId >= #entFactory.recipes and recipeId <= #entFactory.recipes then
 		table.remove(entFactory.recipes, recipeId)
 		--print("delete recipe from list [" .. recipeId .. "]")
 	end
+end
+
+-- Reset the entityfactory
+function entFactory.reset()
+	entFactory.recipes = {}
+end
+
+-- Check if spawnArea is valid
+function entFactory.validateSpawnArea(spawnArea)
+	return spawnArea.x >= 0 and spawnArea.y >= 0 and spawnArea.w <= love.graphics.getWidth() and spawnArea.h <= love.graphics.getHeight();
 end
 
 -- Process recipes
@@ -43,17 +59,11 @@ function entFactory.processRecipes()
 	if #entFactory.recipes > 0 then
 		for i,recipe in pairs(entFactory.recipes) do
 			if recipe.spawnCountDown == 1 then
-				if recipe.amount > 0 then
-					-- Spawn entity
-					ents.Create(recipe.entity, recipe.spawnArea.x, recipe.spawnArea.y + i*50)
-					recipe.spawnArea.x = recipe.spawnArea.x + 25
-					--print("create entity [".. recipe.entity .. "] [" .. recipe.amount .. "] [".. recipe.spawnArea.x .. "] [".. recipe.spawnArea.y .. "]")
-					-- Decrease amount
-					recipe.amount = recipe.amount - 1
-					--print("  recipe.amount [" .. recipe.amount .. "]")
-					-- Reset interval
-					recipe.spawnCountDown = recipe.spawnInterval
-					--print("  reset countdown")
+				if recipe.amount == -1 then
+					-- Special case: unlimited enemies: use reset to stop the factory
+					entFactory.processRecipe(recipe)
+				elseif recipe.amount > 0 then
+					entFactory.processRecipe(recipe)
 				else
 					-- Recipe complete, delete from the list
 					table.insert(delRecipeList, i)
@@ -72,4 +82,30 @@ function entFactory.processRecipes()
 			end
 		end
 	end
+end
+
+function entFactory.processRecipe(recipe)
+	-- Randomize the x,y position within the spawnarea
+	local x,y = entFactory.randCoord(recipe.spawnArea.x, recipe.spawnArea.y, recipe.spawnArea.x + recipe.spawnArea.w, recipe.spawnArea.y + recipe.spawnArea.h)
+	-- Spawn entity
+	ents.Create(recipe.entity, x, y)
+	recipe.spawnArea.x = recipe.spawnArea.x + 25
+	--print("create entity [".. recipe.entity .. "] [" .. recipe.amount .. "] [".. recipe.spawnArea.x .. "] [".. recipe.spawnArea.y .. "]")
+	if recipe.amount ~= -1 then
+		-- Decrease amount
+		recipe.amount = recipe.amount - 1
+	else
+		-- Special case: unlimited enemies
+	end
+	--print("  recipe.amount [" .. recipe.amount .. "]")
+	-- Reset interval
+	recipe.spawnCountDown = recipe.spawnInterval
+	--print("  reset countdown")
+end
+
+-- Coordinate randomizer
+function entFactory.randCoord(x1, y1, x2, y2)
+	local x = math.random(x1, x2)
+	local y = math.random(y1, y2)
+	return x,y;
 end
